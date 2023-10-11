12635c00cdfa79a7d5778a58aeea2f7a4d4a08b4
Pietro907 <pietrop.puri@gmail.com>
2023-10-11 15:24:22 +0200

    add draft style & create input, radios, chk & btn

--- index.php
+++ index.php
@@ -38,25 +38,86 @@ Milestone 4 (BONUS)
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Strong Password Generator</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
-<body>
-    
-<div class="container">
-    <div class="row">
-        <div class="col">
-            <div class="card">
-                
-                
 
+
+<body class="bg-dark">
+
+    <div class="container mt-5 mx-5">
+        <div class="row mt-5">
+            <div class="col mt-5">
+                <div class="card mt-5 p-5">
+
+                    <form action="/index.php" method="GET">
+
+                        <!-- Input text inserisci password -->
+                        <!-- Qui la password viene inserita dall'utente -->
+                        <div class="mb-3 d-flex justify-content-between">
+                            <label for="exampleInputText1" class="form-label">Lunghezza password</label>
+                            <input type="text" class="form-control w-25" id="exampleInputText1" aria-describedby="textHelp">
+                            <!-- <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div> -->
+                        </div>
+
+                        <!-- Qui ci sono i radioboxs -->
+                        <div class="radioboxs">
+
+                        <!-- Radios Si -->
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                            <label class="form-check-label" for="flexRadioDefault1">
+                                Si
+                            </label>
+                        </div>
+
+                        <!-- Radios No -->
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked>
+                            <label class="form-check-label" for="flexRadioDefault2">
+                                No
+                            </label>
+                        </div>
+                        </div>
+
+
+                        <!-- Qui ci sono tutti i checkbox-->
+                        <div class="checkboxs d-flex flex-column justify-content-end align-items-end">
+
+                            <!-- Qui check filtro per Lettere -->
+                            <div class="mb-3 form-check">
+                                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                                <label class="form-check-label" for="exampleCheck1">Lettere</label>
+                            </div>
+                            <!-- Qui check filtro per Numeri -->
+                            <div class="mb-3 form-check">
+                                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                                <label class="form-check-label" for="exampleCheck1">Numeri</label>
+                            </div>
+                            <!-- Qui check filtro per Simboli -->
+                            <div class="mb-3 form-check">
+                                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                                <label class="form-check-label" for="exampleCheck1">Simboli</label>
+                            </div>
+
+                        </div>
+
+
+                        <button type="submit" class="btn btn-primary mt-3">Invia</button>
+                        <button type="reset" class="btn btn-secondary btn mt-3">Annulla</button>
+
+                    </form>
+
+                </div>
             </div>
         </div>
     </div>
-</div>
 
 
 </body>
+
 </html>
