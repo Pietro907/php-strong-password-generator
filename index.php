2c2bc52cf9739a7409cb1fef98396b5260b2e3e2
Pietro907 <pietrop.puri@gmail.com>
2023-10-11 17:11:20 +0200

    add file function.php & insert php logic

--- index.php
+++ index.php
@@ -29,31 +29,8 @@ Milestone 4 (BONUS)
 
 
 <?php
+include __DIR__ . './layout/function.php';
 
-
-
-var_dump($_GET['password']);
-
-function randomPassword() {
-
-    $inputPassw = $_GET['password'];
-    
-    $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!£$%&/^-.;,_<>\|';
-
-    $alphaLength = strlen($alphabet);
-
-    $passw = '';
-
-    for ($i = 0; $i < strlen($inputPassw); $i++) {
-       $randomIndex = random_int(0, $alphaLength);
-
-       $passw .= $alphabet[$randomIndex];
-    }
-
-    return $passw; 
-};
-
-var_dump(randomPassword());
 
 
 ?>
